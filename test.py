2ee37b576aa1684284d4c34f9d0a55b7d2ee5610
Willisp <willispyoung@gmail.com>
2019-07-02 15:35:02 +0800

    measure delay

--- test.py
+++ test.py
@@ -1,24 +1,30 @@
+from ping3 import ping
+
 from find_traces import track
 from find_ips import domain_resolution
 
+
 data = open('data/yt_cdn.txt', 'r')
-res = open('data/yt_ip_hongkong.txt', 'w')
+res = open('data/yt_delay_singapore.txt', 'w')
 
 count = 0
-extra = []
+delays = []
 for line in data.readlines():
     if count > 2:
         domain = line.strip()
-        ips = domain_resolution(domain)
+        delay = ping(domain) * 1000 # ms
+        res.write(str(delay) + "\n")
+        delays.append(delay)
+#        ips = domain_resolution(domain)
 #        print(ips)
-        for ip in ips:
-            res.write(ip + " ")
-        res.write("\n")
-        if len(ips) > 1:
-            extra.append(domain)
+#        for ip in ips:
+#            res.write(ip + " ")
+#        res.write("\n")
+#        if len(ips) > 1:
+#            extra.append(domain)
     count += 1
 
-print(extra)
+#print(extra)
 
 data.close()
 res.close()
