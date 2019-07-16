c6510111a880839306ff151cd56ad89fa01ca00f
willisp <willispyoung@gmail.com>
2019-07-16 16:30:27 +0800

    simple test

--- test.py
+++ test.py
@@ -1,14 +1,3 @@
-import sys
+import glob
 
-file1 = open('cdns1.txt', 'r')
-file2 = open('cdns2.txt', 'r')
-output = open('cdns-out.txt', 'w')
-
-s1 = set([l.strip() for l in file1.readlines()])
-s2 = set([l.strip() for l in file2.readlines()])
-
-res = s1.union(s2)
-for s in res:
-	output.write(s + "\n")
-
-output.close()
+print(glob.glob('data/*/*/*/ips.txt'))
