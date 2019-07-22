aeb2d1c942bf1b68d359999118c69f3f4f48dd55
willisp <willispyoung@gmail.com>
2019-07-22 16:03:02 +0800

    update statistics.py

---
+++ statistics.py
@@ -0,0 +1,69 @@
+import sys
+import glob
+import geoip2.database
+
+from ping3 import ping
+from find_ips import domain_resolution
+
+# CDN statistics.
+
+if len(sys.argv) < 2:
+	print("No command is specified!")
+
+else:
+	flag = sys.argv[1]
+
+	# Statistics on all CDN files for specified location, website and week.
+	if flag == '-default':
+		location = sys.argv[2]
+		website = sys.argv[3]
+		week = sys.argv[4]
+
+		files = glob.glob('data/{}/{}/measure/{}/cdns*.txt'.format(location, website, week))
+
+		cdn_complete_set = {}
+		for file in files:
+			for line in open(file, 'r').readlines():
+				items = line.split(' ')
+				if items[0] in cdn_complete_set:
+					cdn_complete_set[items[0]] += int(items[1])
+				else:
+					cdn_complete_set[items[0]] = int(items[1])
+
+		writer = open('data/{}/{}/statistics/{}/cdn_stats.txt'.format(location, website, week), 'w')
+		
+		for domain in cdn_complete_set:
+			writer.write(domain + "|")
+			writer.write(cdn_complete_set[domian] + "|")
+
+			ips = domain_resolution(domain)
+
+			for i in range(len(ips) - 1):
+				writer.write(ips[i] + ",")
+			writer.write(ips[-1] + "|")
+
+			delays = []
+			for ip in ips:
+				delay = "{:.3f}".format(ping(ip) * 1000)
+				delays.append(delay)
+
+			for i in range(len(delays) - 1):
+				writer.write(delays[i] + ",")
+			writer.write(delays[-1] + "|")
+
+			locations = []
+			reader = geoip2.database.Reader("../GeoLite2-City.mmdb")
+			for ip in ips:
+				result = reader.city(ip)
+				if result.city.name:
+					locations.append(result.city.name)
+				elif result.country.name:
+					locations.append(result.country.name)
+				else:
+					locations.append("unknown")
+
+			for i in range(len(locations) - 1):
+				writer.write(locations[i] + ",")
+			writer.write(locations[-1])
+
+		writer.close()
